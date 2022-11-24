ebd0b8d0343bbf984aee9c530f05e9396c9aa80f
Blake Jakopovic <wakoinc@gmail.com>
2022-11-24 21:28:00 +0100

    rustfmt for main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,8 @@ fn main() {
 
     let cli_matches = cli().get_matches();
 
-    let servers: Vec<String> = cli_matches.get_many::<String>("servers")
+    let servers: Vec<String> = cli_matches
+        .get_many::<String>("servers")
         .unwrap_or_default()
         .map(|v| v.to_string())
         .collect::<Vec<_>>();
@@ -23,17 +24,15 @@ fn main() {
 
     let mut v = Vec::<std::thread::JoinHandle<()>>::new();
     for server in servers {
-      let tx = tx.clone();
-      let input = input.clone();
+        let tx = tx.clone();
+        let input = input.clone();
 
-      log::info!("Spawning thread for -- {}", server);
+        log::info!("Spawning thread for -- {}", server);
 
-      let jh = thread::spawn( move || {
-        run(&tx, server, input, stream)
-      });
+        let jh = thread::spawn(move || run(&tx, server, input, stream));
 
-      v.push(jh);
-    };
+        v.push(jh);
+    }
 
     // drop the original tx, as it was never used
     drop(tx);
@@ -42,19 +41,19 @@ fn main() {
     for line in rx {
 
         match line {
-            Err(e) => { eprintln!("{}", e) },
+            Err(e) => eprintln!("{}", e),
             Ok(line) => {
 
-              if cli_matches.get_flag("unique") {
+                if cli_matches.get_flag("unique") {
 
-                  if seen.contains(&line) {
-                      continue;
-                  }
+                    if seen.contains(&line) {
+                        continue;
+                    }
 
-                  seen.push(line.clone());
-              }
+                    seen.push(line.clone());
+                }
 
-              println!("{}", line);
+                println!("{}", line);
             }
         };
     }
